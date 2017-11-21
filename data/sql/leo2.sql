3635a799d6e49753778819d6a96dbcf1ab73624f
matthewkirby <matthewkirby@email.arizona.edu>
2017-11-21 14:35:20 -0700

    Updated sql script to select on the clean flag and download photometric errors.

--- data/sql/leo2.sql
+++ data/sql/leo2.sql
@@ -4,9 +4,11 @@ DEC: 22.1517
 
 ---------------------------
 SELECT
-  p.ra, p.dec, p.u, p.g, p.r, p.i, p.z
+ p.ra, p.dec, p.psfMag_g, psfMagErr_g, p.psfMag_r, p.psfMagErr_r, p.psfMag_i, p.psfMagErr_i
 FROM PhotoObjAll AS p
 WHERE
   p.ra BETWEEN 167.3708 AND 169.3708
   AND p.dec BETWEEN 21.1517 AND 23.1517
   AND dbo.fDistanceArcMinEq(168.2, 22.1517, p.ra, p.dec) < 30.
+  AND p.clean = 1
+
