cca091f72c46badbade0d249f41794dfc6dba0cf
matthewkirby <matthewkirby@email.arizona.edu>
2017-11-25 13:30:55 -0700

    Changed willman to fits file.

--- data/sql/willman1.sql
+++ data/sql/willman1.sql
@@ -1,7 +1,8 @@
 SELECT
-  p.ra, p.dec, p.u, p.g, p.r, p.i, p.z
+ p.ra, p.dec, p.psfMag_g, psfMagErr_g, p.psfMag_r, p.psfMagErr_r, p.psfMag_i, p.psfMagErr_i
 FROM PhotoObjAll AS p
 WHERE
   p.ra BETWEEN 161.3375 AND 163.3375
   AND p.dec BETWEEN 50.05 AND 52.05
-AND dbo.fDistanceArcMinEq(162.3375, 51.05, p.ra, p.dec) < 30.
+  AND dbo.fDistanceArcMinEq(162.3375, 51.05, p.ra, p.dec) < 30.
+  AND p.clean=1
