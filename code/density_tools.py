d66d6cf5ecc276d0f42b4573ded85fbeaead8013
jngaravitoc <jngaravitoc@email.arizona.edu>
2017-12-01 17:28:01 -0700

    + stupid color bar

--- code/density_tools.py
+++ code/density_tools.py
@@ -15,7 +15,7 @@ TODO:
 
 """
 
-def density_peaks(dens, sigma_t, xmin=0, xmax=1, ymin=0, ymax=1):
+def density_peaks(dens, sigma_min, sigma_max,  xmin=0, xmax=1, ymin=0, ymax=1):
     """
     Function to plot contours on densities:
 
@@ -41,34 +41,38 @@ def density_peaks(dens, sigma_t, xmin=0, xmax=1, ymin=0, ymax=1):
     TODO:
     -----
     1. Fix axes ranges.
-    2. Make label in a for loop.
-    3. Check how to proplerly return a figure.
+    3. Allow to use more than 10 ticklabels in the colorbar
 
     """
     assert xmin < xmax, "xmax should be greater than xmin"
     assert ymin < ymax, "ymax should be greater than ymin"
-    assert sigma_t >0, "sigma_t should be larger than 0"
-    assert type(sigma_t) == int, "sigma_t should be a integer"
+    assert sigma_max >0, "sigma_t should be larger than 0"
+    assert type(sigma_max) == int, "sigma_t should be a integer"
 
     # Defining grid
-    x = np.linspace(xmin, xmax, shape(dens)[0])
-    y = np.linspace(xmin, xmax, shape(dens)[1])
+    x = np.linspace(xmin, xmax, np.shape(dens)[0])
+    y = np.linspace(xmin, xmax, np.shape(dens)[1])
     X, Y = np.meshgrid(x, y)
 
-    # Defining sigma as the standar deviation of the data
+    # Defining sigma as the standard deviation of the data
     sigma = np.std(dens.flatten())
     # Finding the median of the all the data in the field
     dens_median = np.median(dens.flatten())
     # Defining the contours range. 
     overdensities = []
-    for i in range(1, sigma_t+1):
-        overdensities.append(dens_median + i*sigma)
+    color_bar_labels = []
 
+    for i in range(sigma_min, sigma_max+1):
+        overdensities.append(dens_median + i*sigma)
+        color_bar_labels.append(str(i) + '$\sigma$')
+    print(color_bar_labels)
     # Creating contour plot:
     fig = plt.figure(figsize=(6, 6))
     plt.contourf(X, Y, dens, overdensities)
     cbar = plt.colorbar()
-    cbar.ax.set_yticklabels([r'$1\sigma$',r'$2 \sigma$',r'$3 \sigma$',r'$4 \sigma$', r'$5 \sigma$'])
+    cbar.ax.set_yticks(np.arange(0, len(color_bar_labels)+1, 1))
+    cbar.ax.set_yticklabels(color_bar_labels)
+    #fig.clf()
 
     return fig
 
